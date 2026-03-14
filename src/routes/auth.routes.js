ba904528e17e6748a3e3d56c02688650838f249b
Bhoomi Sharma <bhoomi3100@gmail.com>
2026-03-14 19:01:15 +0530

    Add blaclist logout and get me

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const authController = require("../controllers/auth.controller");
+const authMiddleware = require("../middlewares/auth.middleware")
 
 const authRoutes = Router();
 
@@ -16,7 +17,24 @@ authRoutes.post("/register", authController.registerUserController)
  * @description login a with email and password
  * @access Public
  */
-authRoutes.post("/api/login", authController.loginUserController)
+authRoutes.post("/login", authController.loginUserController)
+
+
+
+/**
+ * @route GET /api/auth/logout
+ * @description clear token from user cookie and add token in blacklist
+ * @access Public
+ */
+
+authRoutes.get("/logout", authController.logoutUserController )
+
+/**
+ * @route GET /api/auth/get-me
+ * @description get the current logged in user details
+ * @access private
+ */
+authRoutes.get("/get-me", authMiddleware.authUser, authController.getMeController)
 
 module.exports = authRoutes;
 
